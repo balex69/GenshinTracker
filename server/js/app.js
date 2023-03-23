48798dfcef7b893050e6eabe703e32192bdf87c4
balex69 <119514045+balex69@users.noreply.github.com>
2023-03-23 18:01:24 +0100

    Update app.js

--- server/js/app.js
+++ server/js/app.js
@@ -1,1 +1,27 @@
-console.log("cc");
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database('../../data/data.sqlite', sqlite3.OPEN_READONLY, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the database.');
+});
+
+let data = {};
+db.serialize(() => {
+  db.each("SELECT * FROM SuperTableA", (err, row) => {
+      data[row.id] = {table: "SuperTableA", ...row};
+  });
+  db.each("SELECT * FROM UberTableB", (err, row) => {
+      data[row.id] = {table: "UberTableB", ...row};
+  });
+});
+
+db.close((err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Closed the database connection.');
+});
+
+console.log(JSON.stringify(data));

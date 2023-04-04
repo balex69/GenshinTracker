faa41178391d739d8196ad407a279839c0208cfe
balex69 <119514045+balex69@users.noreply.github.com>
2023-04-04 18:18:07 +0200

    server & ajax

---
+++ public/script.js
@@ -0,0 +1,66 @@
+// Get references to the character and weapon list elements in the HTML
+const rarete5CharactersList = document.getElementById('rarete-5-characters');
+const rarete4CharactersList = document.getElementById('rarete-4-characters');
+const rarete5WeaponsList = document.getElementById('rarete-5-weapons');
+const rarete4WeaponsList = document.getElementById('rarete-4-weapons');
+
+// Function to create an li element for a character or weapon
+function createLiElement(item) {
+  const li = document.createElement('li');
+  li.id = item.id;
+  li.className = `element_${item.type} rarete-${item.rarity} ${item.type}_${item.weaponType}`;
+        
+  const img = document.createElement('img');
+  img.src = item.image;
+  li.appendChild(img);
+        
+  return li;
+}
+
+// AJAX request to retrieve characters with rarity 5
+fetch('/characters/5')
+  .then(response => response.json())
+  .then(characters => {
+    // Create an li element for each character and append it to the list
+    characters.forEach(character => {
+      const li = createLiElement(character);
+      rarete5CharactersList.appendChild(li);
+    });
+  })
+  .catch(error => console.error(error));
+
+// AJAX request to retrieve characters with rarity 4
+fetch('/characters/4')
+  .then(response => response.json())
+  .then(characters => {
+    // Create an li element for each character and append it to the list
+    characters.forEach(character => {
+      const li = createLiElement(character);
+      rarete4CharactersList.appendChild(li);
+    });
+  })
+  .catch(error => console.error(error));
+
+// AJAX request to retrieve weapons with rarity 5
+fetch('/weapons/5')
+  .then(response => response.json())
+  .then(weapons => {
+    // Create an li element for each weapon and append it to the list
+    weapons.forEach(weapon => {
+      const li = createLiElement(weapon);
+      rarete5WeaponsList.appendChild(li);
+    });
+  })
+  .catch(error => console.error(error));
+
+// AJAX request to retrieve weapons with rarity 4
+fetch('/weapons/4')
+  .then(response => response.json())
+  .then(weapons => {
+    // Create an li element for each weapon and append it to the list
+    weapons.forEach(weapon => {
+      const li = createLiElement(weapon);
+      rarete4WeaponsList.appendChild(li);
+    });
+  })
+  .catch(error => console.error(error));
---
+++ server/index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+
+const app = express();
+const port = 3000;
+
+// Connect to the SQLite database
+const db = new sqlite3.Database('./data/data.sqlite', sqlite3.OPEN_READONLY, (err) => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log('Connected to the genshin database.');
+  }
+});
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Handle GET requests to retrieve characters with a specific rarity
+app.get('/characters/:rarity', (req, res) => {
+  const rarity = req.params.rarity;
+  const sql = `SELECT * FROM personnages WHERE rarete_personnage = ?`;
+  
+  db.all(sql, rarity, (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.sendStatus(500);
+    } else {
+      const characters = rows.map(row => ({
+        nom_personnage: row.nom_personnage,
+        rarete_personnage: row.rarete_personnage,
+        arme_personnage: row.arme_personnage,
+        portrait_personnage: row.portrait_personnage
+      }));
+      
+      res.json(characters);
+    }
+  });
+});
+
+// Handle GET requests to retrieve weapons with a specific rarity
+app.get('/weapons/:rarity', (req, res) => {
+  const rarity = req.params.rarity;
+  const sql = `SELECT * FROM armes WHERE rarete_arme = ?`;
+  
+  db.all(sql, rarity, (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.sendStatus(500);
+    } else {
+      const weapons = rows.map(row => ({
+        nom_arme: row.nom_arme,
+        rarete_arme: row.rarete_arme,
+        type_arme: row.type_arme,
+        image_arme: row.image_arme
+      }));
+      
+      res.json(weapons);
+    }
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Genshin Tracker app listening at http://localhost:${port}`);
+});

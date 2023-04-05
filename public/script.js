e0f920a8d2336aa3e6566a799b6e3c3f861b0552
balex69 <119514045+balex69@users.noreply.github.com>
2023-04-05 18:21:56 +0200

    linking & fetching
    values still set as undefined for some reason

--- public/script.js
+++ public/script.js
@@ -1,15 +1,15 @@
 // Get references to the character and weapon list elements in the HTML
-const rarete5CharactersList = document.getElementById('rarete-5-characters');
-const rarete4CharactersList = document.getElementById('rarete-4-characters');
-const rarete5WeaponsList = document.getElementById('rarete-5-weapons');
-const rarete4WeaponsList = document.getElementById('rarete-4-weapons');
+const rarete5CharactersList = document.getElementById('rarete-5-personnages');
+const rarete4CharactersList = document.getElementById('rarete-4-personnages');
+const rarete5WeaponsList = document.getElementById('rarete-5-armes');
+const rarete4WeaponsList = document.getElementById('rarete-4-armes');
 
-// Function to create an li element for a character or weapon
+// Function to create an li element for a character or a weapon
 function createLiElement(item) {
   const li = document.createElement('li');
   li.id = item.id;
-  li.className = `element_${item.type} rarete-${item.rarity} ${item.type}_${item.weaponType}`;
-        
+  li.className = `element_${item.type} rarete-${item.rarity} ${item.weaponType}`;
+        console.log(`element_${item.type} rarete-${item.rarity} ${item.weaponType}`);
   const img = document.createElement('img');
   img.src = item.image;
   li.appendChild(img);
@@ -64,3 +64,74 @@ fetch('/weapons/4')
     });
   })
   .catch(error => console.error(error));
+
+
+/*$(document).ready(function() {
+  get_characters();
+  get_armes();
+});
+
+function get_characters() {
+  $.ajax({
+    url: '/characters/5',
+    method: 'GET',
+    success: function(data) {
+      var characters = JSON.parse(data);
+      var characterList = $('#characters');
+      characterList.empty();
+      for (var i = 0; i < characters.length; i++) {
+        var character = characters[i];
+        var elementClass = character.element_personnage.toLowerCase();
+        var rarityClass = character.rarete_personnage + '-star';
+        var weaponClass = character.arme_personnage.toLowerCase();
+        var name = character.nom_personnage;
+        var imgSrc = character.portrait_personnage;
+        var li = $('<li></li>').attr({
+          'class': 'element_' + elementClass + ' rarete-' + rarityClass + ' ' + weaponClass
+        });
+        var img = $('<img>').attr({
+          'src': imgSrc
+        });
+        li.append(img);
+        li.append(name);
+        characterList.append(li);
+      }
+    },
+    error: function(xhr, status, error) {
+      console.error('Error getting characters:', error);
+    }
+  });
+}
+
+function get_armes() {
+  $.ajax({
+    url: '/weapons/5',
+    method: 'GET',
+    success: function(data) {
+      var armes = JSON.parse(data);
+      var armeList = $('#armes');
+      armeList.empty();
+      for (var i = 0; i < armes.length; i++) {
+        var arme = armes[i];
+        var rarityClass = arme.rarete_arme + '-star';
+        var weaponClass = arme.type_arme.toLowerCase();
+        var name = arme.nom_arme;
+        var imgSrc = arme.portrait_arme;
+        var li = $('<li></li>').attr({
+          'class': 'rarete-' + rarityClass + ' ' + weaponClass
+        });
+        var img = $('<img>').attr({
+          'src': imgSrc
+        });
+        li.append(img);
+        li.append(name);
+        armeList.append(li);
+      }
+    },
+    error: function(xhr, status, error) {
+      console.error('Error getting armes:', error);
+    }
+  });
+}
+
+*/
--- server/index.js
+++ server/index.js
@@ -30,9 +30,75 @@ app.get('/characters/:rarity', (req, res) => {
         nom_personnage: row.nom_personnage,
         rarete_personnage: row.rarete_personnage,
         arme_personnage: row.arme_personnage,
+        portrait_personnage: row.portrait_personnage,
+        element_personnage: row.element_personnage
+      }));
+      res.json(characters);
+    }
+  });
+});
+
+// Handle GET requests to retrieve weapons with a specific rarity
+app.get('/weapons/:rarity', (req, res) => {
+  const rarity = req.params.rarity;
+  const sql = `SELECT * FROM armes WHERE rarete_arme = ?`;
+  
+  db.all(sql, rarity, (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.sendStatus(500);
+    } else {
+      const weapons = rows.map(row => ({
+        nom_arme: row.nom_arme,
+        rarete_arme: row.rarete_arme,
+        type_arme: row.type_arme,
+        portrait_arme: row.portrait_arme
+      }));
+      res.json(weapons);
+    }
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Genshin Tracker app listening at http://localhost:${port}`);
+});
+
+
+/*const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+
+const app = express();
+const port = 3000;
+
+// Connect to the SQLite database
+const db = new sqlite3.Database('./data/data.sqlite', sqlite3.OPEN_READONLY, (err) => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log('Connected to the genshin database.');
+  }
+});
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Handle GET requests to retrieve characters with a specific rarity
+app.get('/characters/:rarity', (req, res) => {
+  const rarity = req.params.rarity;
+  const sql = `SELECT * FROM personnages WHERE rarete_personnage = ?`;
+  
+  db.all(sql, rarity, (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.sendStatus(500);
+    } else {
+      const characters = rows.map(row => ({
+        nom_personnage: row.nom_personnage,
+        rarete_personnage: row.rarete_personnage,
+        arme_personnage: row.arme_personnage,
         portrait_personnage: row.portrait_personnage
       }));
-      
       res.json(characters);
     }
   });
@@ -54,7 +120,6 @@ app.get('/weapons/:rarity', (req, res) => {
         type_arme: row.type_arme,
         image_arme: row.image_arme
       }));
-      
       res.json(weapons);
     }
   });
@@ -63,4 +128,57 @@ app.get('/weapons/:rarity', (req, res) => {
 // Start the server
 app.listen(port, () => {
   console.log(`Genshin Tracker app listening at http://localhost:${port}`);
+});*/
+
+/* V2 ??
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./data/data.sqlite');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  let personnages_rareté_5 = [];
+  let personnages_rareté_4 = [];
+  let armes_rareté_5 = [];
+  let armes_rareté_4 = [];
+
+  // Execute the queries and extract the results
+  db.all('SELECT * FROM personnages WHERE rarete_personnage = 5', function(err, rows) {
+    rows.forEach((row) => {
+      personnages_rareté_5.push(row);
+    });
+  });
+
+  db.all('SELECT * FROM personnages WHERE rarete_personnage = 4', function(err, rows) {
+    rows.forEach((row) => {
+      personnages_rareté_4.push(row);
+    });
+  });
+
+  db.all('SELECT * FROM armes WHERE rarete_arme = 5', function(err, rows) {
+    rows.forEach((row) => {
+      armes_rareté_5.push(row);
+    });
+  });
+
+  db.all('SELECT * FROM armes WHERE rarete_arme = 4', function(err, rows) {
+    rows.forEach((row) => {
+      armes_rareté_4.push(row);
+    });
+    // Pass the extracted data to the view
+    res.render('index', {
+      personnages_rareté_5: personnages_rareté_5,
+      personnages_rareté_4: personnages_rareté_4,
+      armes_rareté_5: armes_rareté_5,
+      armes_rareté_4: armes_rareté_4
+    });
+  });
 });
+
+app.listen(3000, function () {
+  console.log('Listening on http://localhost:3000');
+});
+*/
